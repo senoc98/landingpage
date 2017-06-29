28ebe8b5ba343dbaf1bb6e252b3ef1afe404ae17
Samuel Enocsson <samuel@sammy.se>
2017-06-29 11:08:00 +0200

    Johannes gamla filer
    Lagt till Johannes gamla filer

---
+++ landingpage_sammy/js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+		setBindings();
+	});
+	
+function setBindings() {
+	$("div.section.intro h1.pil a").click(function(e){
+		e.preventDefault();
+		var sectionID = e.currentTarget.id + "Sektion";
+		
+		$("html,body").animate({
+			scrollTop: $("#" + sectionID).offset().top
+			
+		}, 1200, 'easeInOutExpo')
+	})
+  
+  $(".btn2").click(function(e){
+		e.preventDefault();
+		var sectionID = e.currentTarget.id + "Sektion";
+		
+		$("html,body").animate({
+			scrollTop: $("#" + sectionID).offset().top -250
+			
+		}, 1200, 'easeInOutExpo')
+	})
+
+}
+
+  $('.intro').parallax({imageSrc: 'img/mockupbakgrund.png'});
+  
+  window.sr = ScrollReveal();
+  sr.reveal('div.container.main');
+  sr.reveal('div.section.intro h1.pil');
+  sr.reveal('.logo', { origin: 'left'});
+  sr.reveal('.loggainarea', { origin: 'right'});
+  sr.reveal('.container.about');
+  sr.reveal('.teamrubrik');
+  sr.reveal('.oskar', { origin: 'left', delay: 200});
+  sr.reveal('.jan', { origin: 'right', delay: 400});
+  sr.reveal('.samuel', { origin: 'left', delay: 300});
+  sr.reveal('.william', { origin: 'right', delay: 500});
+  sr.reveal('.container.contact');
+
+
+   
